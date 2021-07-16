b63f6d9e0aa78cc60afa0c86c02383ded905034c
shantanu-gupta9617 <shantanu9617@gmail.com>
2021-07-16 19:41:41 +0530

    Added top-section some work is pending

--- src/app/components/navbar/navitems.tsx
+++ src/app/components/navbar/navitems.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import tw from "twin.macro"
-import {Slide as Menu} from "react-burger-menu";
+import {slide as Menu} from "react-burger-menu";
 import {useMediaQuery} from "react-responsive"
+import { SCREENS } from '../responsive';
+import menuStyles from './menuStyles';
 
 const ListContainer=styled.ul`
 ${tw`
@@ -12,9 +14,9 @@ list-none
 `}
 `;
 
-const NavItem=styled.li`
+const NavItem=styled.li<{menu?:any}>`
 ${tw`
-text-xs
+text-sm
 md:text-base
 text-black
 font-medium
@@ -26,10 +28,40 @@ duration-300
 ease-in-out
 hover:text-gray-700
 `}
+
+${({menu})=>menu && css`
+${tw`
+text-white
+text-xl
+mb-3
+focus:text-white
+`}
+`}
 `;
 
 export function NavItems(){
-const isMobile=useMediaQuery({maxWidth: })
+const isMobile=useMediaQuery({maxWidth: SCREENS.sm});
+
+if(isMobile)
+    return(
+        <Menu right styles={menuStyles}>
+     <ListContainer>
+        <NavItem menu>
+            <a href="#">Home</a>
+        </NavItem>
+        <NavItem menu>
+            <a href="#">Cars</a>
+        </NavItem>
+        <NavItem menu>
+            <a href="#">Services</a>
+        </NavItem>
+        <NavItem menu>
+            <a href="#">Contact Us</a>
+        </NavItem>
+    </ListContainer>
+    </Menu>
+    )
+
     return <ListContainer>
         <NavItem>
             <a href="#">Home</a>
